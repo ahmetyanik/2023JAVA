5e026fa08497c740bb6013d03c0358c837839713
ahmetyanik <ahmetyank4242@gmail.com>
2023-01-23 17:19:21 +0100

    for

---
+++ src/main/java/Group01/Deneme.java
@@ -0,0 +1,12 @@
+package Group01;
+
+public class Deneme {
+
+    public static void main(String[] args) {
+
+        String s = "Love";
+        for(int i = s.length()-1; i >= 0; i--){
+            System.out.print(s.charAt(i));
+        }
+    }
+}
