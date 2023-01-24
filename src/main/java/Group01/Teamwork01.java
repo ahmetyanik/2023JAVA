74681d040f05078753d7b55f00ee9a185a2b6e08
ahmetyanik <ahmetyank4242@gmail.com>
2023-01-24 10:03:50 +0100

    sort and abs

--- src/main/java/Group01/Teamwork01.java
+++ src/main/java/Group01/Teamwork01.java
@@ -26,6 +26,27 @@ public class Teamwork01 {
         }
 
 
+        //Find the smallest positive element and greatest negative element in an integer array
+        //Example:(-12, 18, -5, 23, -2) ==> Smallest positive is 18, greatest negative is -2
+
+        int array[] = {-12,18,-5,23,-2,-25,89,78,1,-159,-1};
+
+        Arrays.sort(array);
+
+        System.out.println(Arrays.toString(array));
+
+        int index =-1;
+
+        for(int i=0;i< array.length;i++){
+
+            if(array[i] == Math.abs(array[i])){
+                index = i;
+                break;
+            }
+        }
+
+        System.out.println("Smallest positive is "+ array[index] + ", greatest negative is " + array[index-1]);
+
 
     }
 }
